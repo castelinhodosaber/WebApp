4982744636e7db9633e2ab0fec289761638b86e1
thosijulio <thosijulio@gmail.com>
2024-10-23 13:59:57 -0300

    FEAT - pwa

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 
 function App() {
   useEffect(() => {
@@ -10,12 +10,14 @@ function App() {
     window.addEventListener("beforeinstallprompt", (e: Event) => {
       // Impede que o navegador mostre o prompt de instalação automaticamente
       e.preventDefault();
+      console.log(e);
       deferredPrompt = e;
 
       // Mostre um botão para o usuário optar por instalar
       const installButton = document.getElementById("install-button");
-      if (installButton && deferredPrompt) {
-        installButton.style.display = "block";
+      const installBox = document.getElementById("pwa-prompt");
+      if (installButton && deferredPrompt && installBox) {
+        installBox.style.display = "flex";
 
         installButton.addEventListener("click", () => {
           // Mostra o prompt de instalação quando o botão é clicado
@@ -43,14 +45,53 @@ function App() {
       width="100vw"
       height="100vh"
     >
-      <Button
-        id="install-button"
-        position="absolute"
-        top="1"
-        style={{ display: "none" }}
+      <Flex
+        bgColor="white"
+        borderRadius="8px"
+        boxShadow="0 2px 10px rgba(0, 0, 0, 0.2)"
+        direction="column"
+        padding="10px 20px"
+        color="black"
+        justify="flex-end"
+        position="fixed"
+        top="10"
+        left="50%"
+        transform="translateX(-50%)"
+        zIndex={1000}
+        width={["80%"]}
+        maxW="320px"
+        id="pwa-prompt"
       >
-        Instalar PWA
-      </Button>
+        <Image
+          alt="appLogo"
+          bgColor="#031436"
+          borderRadius="100px"
+          position="absolute"
+          top="2"
+          left="3"
+          src="/assets/icons/icon-512x512.png"
+          width={["50px"]}
+        />
+        <Flex align="" justify="center" direction="column" width="80%">
+          <Text>Agenda Castelinho</Text>
+          <Text>app.castelinhodosaber.com</Text>
+          <Button
+            _hover={{ bgColor: "#0056b3" }}
+            bgColor="#007bff"
+            color="white"
+            border="none"
+            borderRadius="6px"
+            padding="10px 15px"
+            cursor="pointer"
+            fontSize={["16px"]}
+            marginTop="10px"
+            transition="background-color 0.3s"
+            id="install-button"
+          >
+            Adicionar
+          </Button>
+        </Flex>
+      </Flex>
       <Image
         alt="logo"
         src="/assets/icons/icon-512x512.png"
